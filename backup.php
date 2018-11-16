40175c3026091e8e1887d27b39d1d70e7b361c8f
zcg <1394791375@qq.com>
2018-11-16 15:08:59 +0800

    时区问题

--- backup.php
+++ backup.php
@@ -1,11 +1,13 @@
 <?php
+date_default_timezone_set('Asia/Shanghai');
+$dateNow = date('Ymd', (time()-24*60*60));
 
 $leftTxt = file_get_contents('/home/nodePad/left.txt');
 $rightTxt = file_get_contents('/home/nodePad/right.txt');
 
 if($leftTxt)
 {
-    file_put_contents('/home/nodePad/backup/left'.date('Ymd'), $leftTxt);
+    file_put_contents('/home/nodePad/backup/left'.$dateNow, $leftTxt);
 }else
 {
     file_put_contents('/home/nodePad/backup/error.log', time().'------fail\r\n');
@@ -13,7 +15,7 @@ if($leftTxt)
 
 if($rightTxt)
 {
-    file_put_contents('/home/nodePad/backup/right'.date('Ymd'), $rightTxt);
+    file_put_contents('/home/nodePad/backup/right'.$dateNow, $rightTxt);
 }else
 {
     file_put_contents('/home/nodePad/backup/error.log', time().'------fail\r\n');
