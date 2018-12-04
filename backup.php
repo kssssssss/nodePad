3275b8946f05d4fbf995218731ef8c568995d6bb
zcg <1394791375@qq.com>
2018-12-04 11:04:33 +0800

    分文件夹存放

--- backup.php
+++ backup.php
@@ -1,13 +1,15 @@
 <?php
 date_default_timezone_set('Asia/Shanghai');
 $dateNow = date('Ymd', (time()-24*60*60));
+$dirName = date('Ym', (time()-24*60*60));
+mkdir('./'.$dirName);
 
 $leftTxt = file_get_contents('/home/nodePad/left.txt');
 $rightTxt = file_get_contents('/home/nodePad/right.txt');
 
 if($leftTxt)
 {
-    file_put_contents('/home/nodePad/backup/left'.$dateNow, $leftTxt);
+    file_put_contents('/home/nodePad/backup/'.$dirName.'/left'.$dateNow, $leftTxt);
 }else
 {
     file_put_contents('/home/nodePad/backup/error.log', time().'------fail\r\n');
@@ -15,7 +17,7 @@ if($leftTxt)
 
 if($rightTxt)
 {
-    file_put_contents('/home/nodePad/backup/right'.$dateNow, $rightTxt);
+    file_put_contents('/home/nodePad/backup/'.$dirName.'/right'.$dateNow, $rightTxt);
 }else
 {
     file_put_contents('/home/nodePad/backup/error.log', time().'------fail\r\n');
